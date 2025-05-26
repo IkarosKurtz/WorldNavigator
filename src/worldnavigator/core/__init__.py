83520a69f6a08ae38bfa72387e25084347fed765
IkarosKurtz <qwertypopsdown@gmail.com>
2025-05-26 01:21:19 -0600

    feat(character): :sparkles: Add GameCharacter to __all__ exports for improved accessibility

--- src/worldnavigator/core/__init__.py
+++ src/worldnavigator/core/__init__.py
@@ -1,9 +1,11 @@
 from worldnavigator.core.world import World
 from worldnavigator.core.world_parser import WorldParser
 from worldnavigator.core.world_object import WorldObject
+from worldnavigator.core.character import GameCharacter
 
 __all__ = [
   'World',
   'WorldParser',
-  'WorldObject'
+  'WorldObject',
+  'GameCharacter'
 ]
