618bce9e7983c5776d0750c1bf638e9028c0c781
Ikaros <qwertypopsdown@gmail.com>
2025-05-27 13:48:29 -0600

    feat(dev/worldtime): Add time-based conditions and enhance character movement logic

--- dev/testing.py
+++ dev/testing.py
@@ -1,10 +1,11 @@
+import time
 from typing import TypedDict
+
 from worldnavigator.core import GameCharacter, WorldParser
 from worldnavigator.types import BaseCondition
 from worldnavigator.types.types import ConditionPipelineContext
 
 
-# This class is optional, but it's recommended to use it to avoid typos, probably doesn't work in Ren'Py
 class MyData(TypedDict):
   inventory: list[str]
   health: int
@@ -14,12 +15,28 @@ class MyData(TypedDict):
 my_data: MyData = {
   'damage': 10,
   'health': 100,
-  'inventory': ['shield']
+  'inventory': ['sword', 'shield']
 }
 
 human = GameCharacter[MyData]("Human", data=my_data)
 
 world = WorldParser.scene_graph_parser(f'./examples/worlds/nexis.world.json')
+world.character_entrypoint('Main Entrance')
+world.time.adjust_time(20)
+world.time.start_time()
+
+
+# def weather():
+#   while True:
+#     time.sleep(1)
+#     print(world.time.weather.current_weather)
+
+
+# thread = Thread(target=weather, daemon=True)
+# thread.start()
+
+time.sleep(3)
+print(world.time.show_clock())
 
 
 class HasSword(BaseCondition):
@@ -31,17 +48,38 @@ class HasSword(BaseCondition):
     context.denied = True
 
 
-world.character_entrypoint('Main Entrance')
+class IsPassTwelve(BaseCondition):
+  def handle(self, context: ConditionPipelineContext):
+    if world.time.get_time()[0] >= 10:
+      self.handle_next(context)
+      return
+
+    context.denied = True
+
+
+class IsSunny(BaseCondition):
+  def handle(self, context: ConditionPipelineContext):
+    if world.time.weather.current_weather['weather'] == 'Sunny':
+      self.handle_next(context)
+      return
+
+    context.denied = True
+
+
 world.add_character(human)
 print(human.current_location)
 
 school = world.get_location('School')
 
 school.condition_pipeline(
-  HasSword()
+  HasSword(),
+  IsPassTwelve(),
+  IsSunny()
 )
 
-was_moved = world.move_character(human, school)
-print(f'Was moved: {was_moved}')
+while True:
+  was_moved = world.move_character(human, school)
+  print(f'Was moved: {was_moved}')
 
-print(human.current_location)
+  print(human.current_location)
+  time.sleep(1)
