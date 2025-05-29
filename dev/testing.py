a6a73eb81f086a5d30d1705420308ea872c54be1
Ikaros <qwertypopsdown@gmail.com>
2025-05-29 13:28:27 -0600

    feat(dev): Implement weather and time listeners with console output

--- dev/testing.py
+++ dev/testing.py
@@ -1,9 +1,12 @@
 import time
 from typing import TypedDict
+from rich.console import Console
+from rich.markdown import Markdown
+from rich.panel import Panel
 
 from worldnavigator.core import GameCharacter, WorldParser
-from worldnavigator.types import BaseCondition
-from worldnavigator.types.types import ConditionPipelineContext
+
+console = Console()
 
 
 class MyData(TypedDict):
@@ -22,64 +25,49 @@ human = GameCharacter[MyData]("Human", data=my_data)
 
 world = WorldParser.scene_graph_parser(f'./examples/worlds/nexis.world.json')
 world.character_entrypoint('Main Entrance')
-world.time.adjust_time(20)
+world.time.adjust_time(30)
+
+
+def weather(current_step, steps):
+  text = Markdown(
+    "### **Current Weather:**\n{current}\n### **Next Weather:**\n{next}".format(
+      current=current_step["weather"], next=steps[0]["weather"])
+  )
+  console.print(Panel(text, title='Weather'))
+
+
+def thunder():
+  console.print(Panel(Markdown('⚡ ⚡ ⚡ ⚡ ⚡'), title='Thunder'))
+
+
+def updated_time(time):
+  text = Markdown(
+    "### **Current Time:**\n{time}\n### **Current Date:**\n{date}".format(
+      time=world.time.show_clock('12:00'),
+      date=world.time.show_date('DD-MM-YYYY', full_month=True)
+    )
+  )
+  console.print(Panel(text, title='Time'))
+
+
+def updated_date(date):
+  console.print(Panel(Markdown(f'### **Current Date:**\n{date}'), title='Date'))
+
+
+world.time.weather.listen_for_weather_change(weather)
+world.time.weather.listen_for_thunder(thunder)
+world.time.listen_for_time(updated_time)
+world.time.listen_for_date(updated_date)
+
+world.time.weather.override_weather('Stormy', 'Stormy', 15)
+world.time.override_time(20, 0)
+
 world.time.start_time()
 
 
-# def weather():
-#   while True:
-#     time.sleep(1)
-#     print(world.time.weather.current_weather)
-
-
-# thread = Thread(target=weather, daemon=True)
-# thread.start()
-
-time.sleep(3)
-print(world.time.show_clock())
-
-
-class HasSword(BaseCondition):
-  def handle(self, context: ConditionPipelineContext):
-    if 'sword' in human.data['inventory']:
-      self.handle_next(context)
-      return
-
-    context.denied = True
-
-
-class IsPassTwelve(BaseCondition):
-  def handle(self, context: ConditionPipelineContext):
-    if world.time.get_time()[0] >= 10:
-      self.handle_next(context)
-      return
-
-    context.denied = True
-
-
-class IsSunny(BaseCondition):
-  def handle(self, context: ConditionPipelineContext):
-    if world.time.weather.current_weather['weather'] == 'Sunny':
-      self.handle_next(context)
-      return
-
-    context.denied = True
-
-
-world.add_character(human)
-print(human.current_location)
-
-school = world.get_location('School')
-
-school.condition_pipeline(
-  HasSword(),
-  IsPassTwelve(),
-  IsSunny()
-)
-
+idx = 0
 while True:
-  was_moved = world.move_character(human, school)
-  print(f'Was moved: {was_moved}')
-
-  print(human.current_location)
   time.sleep(1)
+  if idx == 15:
+    world.time.weather.throw_thunder()
+  idx += 1
